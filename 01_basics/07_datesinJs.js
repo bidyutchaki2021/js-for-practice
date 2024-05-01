2d58668b47027a1c9b6646ebd2f50bf9ce625664
chakibidyut <132157634+bidyutchaki2021@users.noreply.github.com>
2024-05-01 05:53:43 +0000

    create 07_datesinJs

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,26 @@
+//Dates
+
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+//let myCreatedDate=new Date(2023,1,23,5,3)
+// let myCreatedDate=new Date("2024-05-01")
+ let myCreatedDate=new Date("01-05-2024")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+
+newDate.toLocaleDateString('default',{
+    weekday:"long"
+})
+
