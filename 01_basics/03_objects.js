430cad6a91efce71c1bdd667970d7aeabf7c7e96
chakibidyut <132157634+bidyutchaki2021@users.noreply.github.com>
2024-05-01 14:48:54 +0000

    object creation

---
+++ 01_basics/03_objects.js
@@ -0,0 +1,33 @@
+//singleton
+//object.create
+
+//object literals
+const mySym=Symbol("key1")
+const JsUser={
+    name:"Bidyut",
+    "full name":"bidyut chaki",
+    [mySym]:"mykey1",
+    age:18,
+    location:"kolkata",
+    email:"bidyut@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Friday"]
+}
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email="bidyut@yahoo.com"
+//Object.freeze(JsUser)
+JsUser.email="bidyut@outlook.com"
+console.log(JsUser);
+
+JsUser.greeting=function(){
+    console.log("Hello JS user");
+}
+JsUser.greeting2=function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greeting2());
